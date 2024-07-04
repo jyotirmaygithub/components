162454716553b00c5fca16b898d7382c3a651514
jyotirmay <jyotirmay2000gupta@gmail.com>
2024-07-04 22:26:55 +0530

    Feat :  add product

--- src/components/addProduct.js
+++ src/components/addProduct.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { ChevronDownIcon } from '@heroicons/react/24/solid';
 
 const AddProduct = () => {
   const [productName, setProductName] = useState('');
@@ -31,214 +32,299 @@ const AddProduct = () => {
   };
 
   return (
-    <div className="p-6 bg-gray-100">
-      <h1 className="text-2xl font-bold mb-4">Add Product</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div className='p-6 bg-gray-100 m-5 rounded-lg'>
+      <h1 className='text-2xl font-bold mb-4'>Add Product</h1>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
         <div>
-          <label className="block text-sm font-medium mb-1">Product Name</label>
+          <label className='block text-sm font-medium mb-1'>Product Name</label>
           <input
-            type="text"
+            className='w-full p-2 border border-transparent rounded focus:outline-none  focus:border-focusBorder'
+            type='text'
             value={productName}
             onChange={(e) => setProductName(e.target.value)}
-            maxLength="30"
-            className="focus:ring-custom focus:ring-2 w-full p-2 border border-transparent focus:border-blue-500 rounded"
+            maxLength='30'
           />
         </div>
-        <div>
-          <label className="block text-sm font-medium mb-1">Category</label>
-          <select
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
+        <div className='w-72'>
+          <label
+            className='block text-gray-700 text-sm font-bold mb-2'
           >
-            <option value="">Select</option>
-            {/* Add options here */}
-          </select>
+            Select Version
+          </label>
+          <div className='relative'>
+            <select
+              id='category'
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              className='block appearance-none w-full bg-white border border-transparent px-4 py-2 pr-10 rounded focus:outline-none focus:border-focusBorder '
+            >
+              <option value=''>Select an option</option>
+              <option value='Material Tailwind HTML'>
+                Material Tailwind HTML
+              </option>
+              <option
+                value='Material Tailwind React'
+              >
+                Material Tailwind React
+              </option>
+            </select>
+            <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700'>
+              <ChevronDownIcon className='h-5 w-5' />
+            </div>
+          </div>
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Gender</label>
-          <select
-            value={gender}
-            onChange={(e) => setGender(e.target.value)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
-          >
-            <option value="">Select</option>
-            {/* Add options here */}
-          </select>
+          <label className='block text-sm font-medium mb-1'>Gender</label>
+          <div className='relative'>
+            <select
+              id='gender'
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
+              className='block appearance-none w-full bg-white border border-transparent px-4 py-2 pr-10 rounded focus:outline-none focus:border-focusBorder '
+            >
+              <option value=''>Select an option</option>
+              <option value='Material Tailwind HTML'>
+                Material Tailwind HTML
+              </option>
+              <option
+                value='Material Tailwind React'
+              >
+                Material Tailwind React
+              </option>
+            </select>
+            <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700'>
+              <ChevronDownIcon className='h-5 w-5' />
+            </div>
+          </div>
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Size</label>
-          <select
-            value={size}
-            onChange={(e) => setSize(e.target.value)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
-          >
-            <option value="">Select</option>
-            {/* Add options here */}
-          </select>
+          <label className='block text-sm font-medium mb-1'>Size</label>
+          <div className='relative'>
+            <select
+              id='size'
+              value={size}
+              onChange={(e) => setSize(e.target.value)}
+              className='block appearance-none w-full bg-white border border-transparent px-4 py-2 pr-10 rounded focus:outline-none focus:border-focusBorder '
+            >
+              <option value=''>Select an option</option>
+              <option value='Material Tailwind HTML'>
+                Material Tailwind HTML
+              </option>
+              <option
+                value='Material Tailwind React'
+              >
+                Material Tailwind React
+              </option>
+            </select>
+            <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700'>
+              <ChevronDownIcon className='h-5 w-5' />
+            </div>
+          </div>
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Brand</label>
-          <select
-            value={brand}
-            onChange={(e) => setBrand(e.target.value)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
-          >
-            <option value="">Select</option>
-            {/* Add options here */}
-          </select>
+          <label className='block text-sm font-medium mb-1'>Brand</label>
+          <div className='relative'>
+            <select
+              id='brand'
+              value={brand}
+              onChange={(e) => setBrand(e.target.value)}
+              className='block appearance-none w-full bg-white border border-transparent px-4 py-2 pr-10 rounded focus:outline-none focus:border-focusBorder '
+            >
+              <option value=''>Select an option</option>
+              <option value='Material Tailwind HTML'>
+                Material Tailwind HTML
+              </option>
+              <option
+                value='Material Tailwind React'
+              >
+                Material Tailwind React
+              </option>
+            </select>
+            <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700'>
+              <ChevronDownIcon className='h-5 w-5' />
+            </div>
+          </div>
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Colors</label>
+          <label className='block text-sm font-medium mb-1'>Colors</label>
           <input
-            type="text"
+            type='text'
             value={colors}
             onChange={(e) => setColors(e.target.value)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
+            className='w-full p-2 border border-transparent rounded focus:outline-none  focus:border-focusBorder'
           />
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Cost</label>
+          <label className='block text-sm font-medium mb-1'>Cost</label>
           <input
-            type="number"
+            type='number'
             value={cost}
             onChange={(e) => setCost(e.target.value)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
+            className='w-full p-2 border border-transparent rounded focus:outline-none  focus:border-focusBorder'
           />
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Product Description</label>
+          <label className='block text-sm font-medium mb-1'>
+            Product Description
+          </label>
           <textarea
             value={productDescription}
             onChange={(e) => setProductDescription(e.target.value)}
-            maxLength="500"
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
+            maxLength='500'
+            className='w-full p-2 border border-transparent rounded focus:outline-none  focus:border-focusBorder'
           ></textarea>
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Product Features</label>
+          <label className='block text-sm font-medium mb-1'>
+            Product Features
+          </label>
           <ReactQuill
             value={productFeatures}
             onChange={setProductFeatures}
-            className="w-full border border-transparent focus-within:border-blue-500 rounded"
+            className='w-full border border-transparent focus-within:border-blue-500 rounded'
           />
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Actual Price</label>
+          <label className='block text-sm font-medium mb-1'>Actual Price</label>
           <input
-            type="number"
+            type='number'
             value={actualPrice}
             onChange={(e) => setActualPrice(e.target.value)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
+            className='w-full p-2 border border-transparent rounded focus:outline-none  focus:border-focusBorder'
           />
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Dealer Price</label>
+          <label className='block text-sm font-medium mb-1'>Dealer Price</label>
           <input
-            type="number"
+            type='number'
             value={dealerPrice}
             onChange={(e) => setDealerPrice(e.target.value)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
+            className='w-full p-2 border border-transparent rounded focus:outline-none  focus:border-focusBorder'
           />
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Discount</label>
+          <label className='block text-sm font-medium mb-1'>Discount</label>
           <input
-            type="number"
+            type='number'
             value={discount}
             onChange={(e) => setDiscount(e.target.value)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
+            className='w-full p-2 border border-transparent rounded focus:outline-none  focus:border-focusBorder'
           />
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Product Type</label>
+          <label className='block text-sm font-medium mb-1'>Product Type</label>
           <input
-            type="text"
+            type='text'
             value={productType}
             onChange={(e) => setProductType(e.target.value)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
+            className='w-full p-2 border border-transparent rounded focus:outline-none  focus:border-focusBorder'
           />
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Item Weight (in gms)</label>
+          <label className='block text-sm font-medium mb-1'>
+            Item Weight (in gms)
+          </label>
           <input
-            type="number"
+            type='number'
             value={itemWeight}
             onChange={(e) => setItemWeight(e.target.value)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
+            className='w-full p-2 border border-transparent rounded focus:outline-none  focus:border-focusBorder'
           />
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Product Images</label>
+          <label className='block text-sm font-medium mb-1'>
+            Product Images
+          </label>
           <input
-            type="file"
+            type='file'
             multiple
             onChange={(e) => handleImageUpload(e, setProductImages)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
+            className='w-full p-2 border border-transparent focus:border-blue-500 rounded'
           />
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Warranty Documents</label>
+          <label className='block text-sm font-medium mb-1'>
+            Warranty Documents
+          </label>
           <input
-            type="file"
+            type='file'
             multiple
             onChange={(e) => handleImageUpload(e, setWarrantyDocuments)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
+            className='w-full p-2 border border-transparent rounded focus:outline-none  focus:border-focusBorder'
           />
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Publish Date</label>
+          <label className='block text-sm font-medium mb-1'>Publish Date</label>
           <input
-            type="date"
+            type='date'
             value={publishDate}
             onChange={(e) => setPublishDate(e.target.value)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
+            className='w-full p-2 border border-transparent rounded focus:outline-none  focus:border-focusBorder'
           />
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Publish Time</label>
+          <label className='block text-sm font-medium mb-1'>Publish Time</label>
           <input
-            type="time"
+            type='time'
             value={publishTime}
             onChange={(e) => setPublishTime(e.target.value)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
+            className='w-full p-2 border border-transparent rounded focus:outline-none  focus:border-focusBorder'
           />
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Published Status</label>
-          <select
-            value={publishedStatus}
-            onChange={(e) => setPublishedStatus(e.target.value)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
-          >
-            <option value="">Select</option>
-            {/* Add options here */}
-          </select>
+          <label className='block text-sm font-medium mb-1'>
+            Published Status
+          </label>
+          <div className='relative'>
+            <select
+              id='publishedStatus'
+              value={publishedStatus}
+              onChange={(e) => setPublishedStatus(e.target.value)}
+              className='block appearance-none w-full bg-white border border-transparent px-4 py-2 pr-10 rounded focus:outline-none focus:border-focusBorder '
+            >
+              <option value=''>Select an option</option>
+              <option value='Material Tailwind HTML'>
+                Material Tailwind HTML
+              </option>
+              <option
+                value='Material Tailwind React'
+              >
+                Material Tailwind React
+              </option>
+            </select>
+            <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700'>
+              <ChevronDownIcon className='h-5 w-5' />
+            </div>
+          </div>
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Product Tags</label>
+          <label className='block text-sm font-medium mb-1'>Product Tags</label>
           <input
-            type="text"
+            type='text'
             value={productTags}
             onChange={(e) => setProductTags(e.target.value)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
+            className='w-full p-2 border border-transparent rounded focus:outline-none  focus:border-focusBorder'
           />
         </div>
         <div>
-          <label className="block text-sm font-medium mb-1">Availability</label>
+          <label className='block text-sm font-medium mb-1'>Availability</label>
           <select
             value={availability}
             onChange={(e) => setAvailability(e.target.value)}
-            className="w-full p-2 border border-transparent focus:border-blue-500 rounded"
+            className='w-full p-2 border border-transparent focus:border-blue-500 rounded'
           >
-            <option value="">Select</option>
+            <option value=''>Select</option>
             {/* Add options here */}
           </select>
         </div>
       </div>
-      <div className="mt-4">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded mr-2">Add Product</button>
-        <button className="bg-gray-500 text-white px-4 py-2 rounded">Save Product</button>
+      <div className='mt-4'>
+        <button className='bg-blue-500 text-white px-4 py-2 rounded mr-2'>
+          Add Product
+        </button>
+        <button className='bg-gray-500 text-white px-4 py-2 rounded'>
+          Save Product
+        </button>
       </div>
     </div>
   );
